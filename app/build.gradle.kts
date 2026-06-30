f65471f2d9ffa9ca4ed9192a15f5d13a394521f2
Aurélien Mombelli <mombelli.aurelien@gmail.com>
2026-06-30 14:44:30 +0200

    chore: bump app version for v2.7.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "fr.aumombelli.dstcg"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.7.8"
+        versionCode = 14
+        versionName = "2.7.18"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
